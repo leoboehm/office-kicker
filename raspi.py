0163d86bf8293d925aba17cb7a4cce9c45de60af
boehm <boehm@quellwerke.de>
2024-07-16 10:45:30 +0200

    ADD: added raspi file for code review

---
+++ raspi.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+
+# GPIO setup
+PIR_PIN = 11    # TODO: adjust to actual sensor pin
+LED_PIN = 13    # TODO: adjust to actual LED pin
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+SERVER_URL = 'https://58457-3000.4.codesphere.com/motion'
+
+def detect_motion():
+    while True:
+        if GPIO.input(PIR_PIN):
+            print("Motion Detected!")
+            # turn on LED
+            GPIO.output(LED_PIN, GPIO.HIGH)
+            try:
+                requests.post(SERVER_URL, json={'motion': True})    # post or get?
+            except Exception as e:
+                print(f"Failed to send data: {e}")
+        else:
+            print("No Motion")
+            # turn off LED
+            GPIO.output(LED_PIN, GPIO.LOW)
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        detect_motion()
+    except KeyboardInterrupt:
+        print("Quit")
+        GPIO.cleanup()
