45885c278e4b32428f7a9cfbed090a821072db50
boehm <boehm@quellwerke.de>
2024-07-17 12:54:10 +0200

    FIX: changed code to receive motion state instead of setting timestamp

--- raspi.py
+++ raspi.py
@@ -3,31 +3,46 @@ import time
 import requests
 
 # GPIO setup
-PIR_PIN = 11    # TODO: adjust to actual sensor pin
-LED_PIN = 13    # TODO: adjust to actual LED pin
+PIR_PIN = 11  # TODO: adapt to actual sensor pin
+LED_PIN = 13  # TODO: adapt to actual LED pin
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIR_PIN, GPIO.IN)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-SERVER_URL = 'https://58457-3000.4.codesphere.com/motion'
+# server url
+FLASK_SERVER_URL = 'https://58457-3000.4.codesphere.com/motion'
+
 
 def detect_motion():
     while True:
+        
+        # check for motion
         if GPIO.input(PIR_PIN):
             # turn on LED
             GPIO.output(LED_PIN, GPIO.HIGH)
             try:
-                requests.post(SERVER_URL, json={'motion': True})    # post or get?
+                # set motion state to true
+                requests.post(FLASK_SERVER_URL, json={'motion': True})
             except Exception as e:
                 print(f"Failed to send data: {e}")
+
         else:
             # turn off LED
             GPIO.output(LED_PIN, GPIO.LOW)
+            try:
+                # set motion state to false
+                requests.post(FLASK_SERVER_URL, json={'motion': False}) 
+            except Exception as e:
+                print(f"Failed to send data: {e}")
+        
+        # update every 10 secs
+        time.sleep(10)
 
-        time.sleep(30)
 
 if __name__ == "__main__":
     try:
         detect_motion()
     except KeyboardInterrupt:
+        print("Quit")
         GPIO.cleanup()
--- server.py
+++ server.py
@@ -1,36 +1,30 @@
-from flask import Flask, jsonify, render_template_string, render_template
+from flask import Flask, jsonify, request, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
-timestamp = None
+motion_state = {'motion': False}
 
-# render ui template based on motion activity
 @app.route("/")
 def index():
-    if get_motion_timedout():
-        return render_template("sleepMode.html")
-    else:
+    if get_motion() == True:
         return render_template("actionMode.html")
+    else:
+        return render_template("sleepMode.html")
 
-# set last motion timestamp
 @app.route('/motion', methods=['GET'])
-def set_motion_timestamp():
-    global timestamp
-    timestamp = datetime.now()
-    return jsonify({"message": "Timestamp set", "timestamp": timestamp}), 200
+def get_motion():
+    return jsonify(motion_state)
 
+@app.route('/motion', methods=['POST'])
+def update_motion():
+    global motion_state
+    data = request.json
+    if 'motion' in data:
+        motion_state['motion'] = data['motion']
+        return jsonify(success=True), 200
+    else:
+        return jsonify(success=False, error="Invalid data"), 400
 
-# check for recent motion activity
-def get_motion_timedout():
-    global timestamp
-    # no motion activity detected
-    if timestamp is None:
-        return True
-    # last motion activity was longer than 5min ago
-    delta = datetime.now() - timestamp
-    print(delta.total_seconds())
-    if delta.total_seconds() > 300:
-        return True
-    # recent motion activity detected
-    return False
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
