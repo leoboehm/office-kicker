bf8f596b8a493c1761679ba1bda4b7666e84f0fd
boehm <boehm@quellwerke.de>
2024-07-18 22:35:14 +0200

    FIX: minor code improvements, added comments, renamed files

--- kicker_pi.py
+++ kicker_pi.py
@@ -3,15 +3,15 @@ import time
 import requests
 
 # GPIO setup
-PIR_PIN = 11  # TODO: adapt to actual sensor pin
-LED_PIN = 13  # TODO: adapt to actual LED pin
+PIR_PIN = 17
+LED_PIN = 18
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIR_PIN, GPIO.IN)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 # server url
-FLASK_SERVER_URL = 'https://58457-3000.4.codesphere.com/motion'
+SERVER_URL = 'https://58457-3000.4.codesphere.com/motion'
 
 
 def detect_motion():
@@ -23,7 +23,7 @@ def detect_motion():
             GPIO.output(LED_PIN, GPIO.HIGH)
             try:
                 # set motion state to true
-                requests.post(FLASK_SERVER_URL, json={'motion': True})
+                requests.post(SERVER_URL, json={'motion': True})
             except Exception as e:
                 print(f"Failed to send data: {e}")
 
@@ -32,7 +32,7 @@ def detect_motion():
             GPIO.output(LED_PIN, GPIO.LOW)
             try:
                 # set motion state to false
-                requests.post(FLASK_SERVER_URL, json={'motion': False}) 
+                requests.post(SERVER_URL, json={'motion': False}) 
             except Exception as e:
                 print(f"Failed to send data: {e}")
         
--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 from flask import Flask, jsonify, request, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 
 motion_state = {'motion': False}
 
+# render template based on motion state
 @app.route("/")
 def index():
     if get_motion() == True:
-        return render_template("actionMode.html")
+        return render_template("occupied.html")
     else:
-        return render_template("sleepMode.html")
+        return render_template("unoccupied.html")
 
+# return current motion state
 @app.route('/motion', methods=['GET'])
 def get_motion():
     return jsonify(motion_state)
 
+# raspi endpoint to set the current motion state
 @app.route('/motion', methods=['POST'])
 def update_motion():
     global motion_state
